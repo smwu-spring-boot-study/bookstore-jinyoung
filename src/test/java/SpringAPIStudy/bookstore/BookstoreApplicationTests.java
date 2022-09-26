e50bd7995d5f137d58b11d22af01fa3970b4f825
young <jinyoung7165@gmail.com>
2022-09-26 22:12:40 +0900

    [ADD]Initial settings

---
+++ src/test/java/SpringAPIStudy/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringAPIStudy.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
