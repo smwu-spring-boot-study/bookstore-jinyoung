09c34beffc5df59a8c48c67426803269b533f42c
young <jinyoung7165@gmail.com>
2022-10-25 22:19:41 +0900

    [FEAT] Swagger 3.0

--- src/main/java/SpringAPIStudy/bookstore/BookstoreApplication.java
+++ src/main/java/SpringAPIStudy/bookstore/BookstoreApplication.java
@@ -2,14 +2,11 @@ package SpringAPIStudy.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 
 import javax.annotation.PostConstruct;
-import java.util.Locale;
 import java.util.TimeZone;
 
 @SpringBootApplication
--- src/main/java/SpringAPIStudy/bookstore/config/SwaggerConfig.java
+++ src/main/java/SpringAPIStudy/bookstore/config/SwaggerConfig.java
@@ -1,34 +1,38 @@
 package SpringAPIStudy.bookstore.config;
 
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfiguration {
+@EnableWebMvc
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket restAPI() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    public Docket api(TypeResolver typeResolver) {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("SpringAPIStudy.bookstore"))
                 .paths(PathSelectors.any())
                 .build();
+
     }
 
-    private ApiInfo apiInfo() {
+   private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Bookstore Spring Boot REST API")
-                .version("1.0.0")
-                .description("Bookstore swagger api 입니다.")
+                .title("BookStore API 명세서")
+                .description("API 공부")
+                .version("1.0")
                 .build();
     }
 }
